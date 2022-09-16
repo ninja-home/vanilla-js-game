b68f79634f3ba2bf56986b4b9d094db651b57ffd
ninja-home <sergio.pinilos.dev@gmail.com>
2022-09-16 16:37:16 -0500

    fix:webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,30 @@ module.exports = (env, options) => {
       rules: [...rules.files, rules.scripts, rules.styles]
     },
     plugins: [plugins.html, plugins.images, plugins.extractStyles, plugins.purgeStyles],
-    
-    
+    devServer: {
+      open: true,
+      port: 4000,
+      https: false,
+      hot: true,
+      historyApiFallback: true,
+      watchOptions: {
+        poll: true
+      }
+      // proxy: { '/api': 'http://localhost:3000' }
+    },
+    optimization: {
+      minimizer: [plugins.uglify],
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            chunks: 'all',
+            test: path.resolve(__dirname, 'node_modules'),
+            name: 'vendor',
+            enforce: true
+          }
+        }
+      }
+    },
     resolve: {
       alias: {
         assets: path.resolve(__dirname, 'src/assets'),
